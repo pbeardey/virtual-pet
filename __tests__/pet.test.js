3b8401f926644265f1f2beb1117ca14d67f445fa
Paul Beard <pbeardey@gmail.com>
2022-03-05 10:58:21 +0000

    add growUp test and method

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,12 +1,27 @@
 const Pet = require('../src/pet');
 
 describe('constructor',() => {
+   
+   
     it ('returns an object', () => {
         expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
     it('sets the name property', () => {
         const pet = new Pet('Fido');
-        
         expect(pet.name).toEqual('Fido');
     })
+    it('checks the age property', () => {
+        const pet = new Pet('Fido');
+        expect(pet.age).toEqual(0);
+    })
+    
+    const pet = new Pet('Fido');
+
+    it('use the growUp method to increase the age by 1', () => {
+        expect(pet.age).toEqual(0);
+        pet.growUp();
+        expect(pet.age).toEqual(1);
+
+    })
+
 });
--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,13 @@
 function Pet(name) {
     this.name = name;
+    this.age = 0;
 }
 
-module.exports = Pet;
+Pet.prototype.growUp = function () {
+    //This method increases the age property by 1
+    this.age += 1;
+}
+
+
+
+module.exports = Pet; 
