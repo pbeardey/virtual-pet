b94eba1d47e0ff1bbd47d89db4295ca806366d3c
Paul Beard <pbeardey@gmail.com>
2022-03-05 15:00:48 +0000

    add hunger property and increment to Pet

--- src/pet.js
+++ src/pet.js
@@ -1,11 +1,13 @@
 function Pet(name) {
     this.name = name;
     this.age = 0;
+    this.hunger = 0;
 }
 
 Pet.prototype.growUp = function () {
     //This method increases the age property by 1
     this.age += 1;
+    this.hunger +=5;
 }
 
 
